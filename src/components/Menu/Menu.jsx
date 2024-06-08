4a96e5affcfbb4846efd4d73fb452e9a21486a06
debora abarca <abarcazavalad@gmail.com>
2024-06-07 20:58:05 -0500

    second commit

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,23 +1,44 @@
-import { useState } from "react"
-import styles from './Menu.module.css'
+import { useState, useEffect, useRef } from "react";
+import styles from './Menu.module.css';
+
 export default function Menu() {
-  const [open, setOpen] = useState(false)
-  function handleOpen(){
-      setOpen(!open)
-    }
+  const [open, setOpen] = useState(false);
+  const menuRef = useRef(null);
+
+  function handleOpen() {
+    setOpen(true);
+  }
+
+  function handleClose() {
+    setOpen(false);
+  }
+
+  useEffect(() => {
+      function handleClickOutside(event) {
+        if (menuRef.current && !menuRef.current.contains(event.target)) {
+          setOpen(!open);
+        }
+      }
+
+      document.addEventListener("mousedown", handleClickOutside);
+      return () => {
+        document.removeEventListener("mousedown", handleClickOutside);
+      };
+    }, [menuRef, open]);
 
   return (
-    <div className={styles.menu}>
-      <button className={styles.button} onClick={handleOpen}>Actions</button>
-      {open && <div>
-        <ul className={styles.menuButton} id="menuButton">
-          <li onClick={handleOpen} className={styles.li}>One</li>
-          <li onClick={handleOpen} className={styles.li}>Two</li>
-          <li onClick={handleOpen} className={styles.li}>Three</li>
-          <li onClick={handleOpen}  className={styles.delete}>Delete</li>
-        </ul>
-      </div>}
-      </div>
+    <div className={styles.menu} >
+      <button className={styles.button} ref={menuRef} onClick={handleOpen}>Actions</button>
+        <div>
+          <ul
+            className={open ? `${styles.menuButton} ${styles.open}` : styles.menuButton }
+          >
+            <li onClick={handleClose} className={styles.li}>One</li>
+            <li onClick={handleClose} className={styles.li}>Two</li>
+            <li onClick={handleClose} className={styles.li}>Three</li>
+            <li onClick={handleClose} className={styles.delete}>Delete</li>
+          </ul>
+        </div>
+    </div>
   );
 }
-
